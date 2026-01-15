27da31eb5c94fa8d351006d57a3a60c8b06665f8
dominik <dominikxmichalik@gmail.com>
2026-01-15 10:06:44 -0400

    gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig = {
   output: "export",
   basePath: isProd ? "/gpredict" : "",
-  assetPrefix: isProd ? "/gpredict/" : "",
   images: {
     unoptimized: true,
   },
