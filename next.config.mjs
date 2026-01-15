fe0c38f98ab05af007f7f053a70f16e40c7b9c88
dominik <dominikxmichalik@gmail.com>
2026-01-15 09:55:47 -0400

    gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,10 @@ const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig = {
   output: "export",
-
-  // REQUIRED for GitHub Pages
-  trailingSlash: true,
-
   basePath: isProd ? "/gpredict" : "",
   assetPrefix: isProd ? "/gpredict/" : "",
-
   images: {
     unoptimized: true,
-  },
-
-  typescript: {
-    ignoreBuildErrors: true,
   },
 }
 
